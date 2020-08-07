7d98817eeb503a20a907235138be443e7e86d825
ReginaF2012 <skritschy@gmail.com>
2020-08-07 12:46:21 -0500

    update route is working

--- app/controllers/dtomos_controller.rb
+++ app/controllers/dtomos_controller.rb
@@ -5,4 +5,16 @@ class DtomosController < ApplicationController
         render json: DtomoSerializer.new(dtomos).to_serialized_json
     end
 
+    def update
+        dtomo = Dtomo.find_by(id: params[:dottomodachi][:id])
+        dtomo.update(dtomo_params)
+        render json: DtomoSerializer.new(dtomo).to_serialized_json
+    end
+
+    private
+
+    def dtomo_params
+        params.require(:dottomodachi).permit(:name, :user_id, :happiness_meter, :hunger_meter, :weight_meter, :total_points, :evolution_countdown, :stage, :evo_type)
+    end
+
 end
