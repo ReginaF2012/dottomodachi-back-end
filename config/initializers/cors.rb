d289a43a560bde7e4125a06157d1d03227c39d02
ReginaF2012 <skritschy@gmail.com>
2020-08-10 18:04:41 -0500

    changed cors, trying to get it to allow access to deployed front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     
   allow do
-    origins 'https://reginaf2012.github.io/dottomodachi-front-end/'
+    origins 'https://reginaf2012.github.io/'
 
     resource '*',
 
