bea8b436c6c2d17fca701dedbba88acbd38b082d
ReginaF2012 <skritschy@gmail.com>
2020-08-10 20:50:08 -0500

    still trying to get heroku to work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
     # the token to be given to a user upon successful signup/login
     def encode_token(payload)
-      JWT.encode(payload, process.env.JWT_SECRET_KEY)
+      JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
   
     # Since token is sent back in a header this is how we access the token
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::API
         # The Begin/Rescue syntax allows us to rescue out of an exception in Ruby.
         begin
           # This will decode the payload we originally gave the token
-          JWT.decode(token, process.env.JWT_SECRET_KEY, true, algorithm: 'HS256')
+          JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256')
         rescue JWT::DecodeError
           nil
         end
