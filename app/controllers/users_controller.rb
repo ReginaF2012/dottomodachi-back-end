71395dfb5de787f4a7698557b31a403d3d5f0d25
ReginaF2012 <skritschy@gmail.com>
2020-08-14 22:47:27 -0500

    trying to fix user create errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
     skip_before_action :authorized, only: [:create]
 
     def create
-      user = User.create(user_params)
-      if user.valid?
+      user = User.new(user_params)
+      if user.save
         token = encode_token(user_id: user.id)
         render json: { userdata: UserSerializer.new(user), jwt: token }, status: :created
       else
